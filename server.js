e1ced39dd3867682af5e84e21533961d8bf2c13b
Praise Ebuka <praise.njoga@gmail.com>
2023-09-03 19:03:18 +0100

    registration's set

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+// Importing preinstalled libraries
 const express = require("express");
 const app = express();
-
+const bcrypt = require("bcrypt");
 const port = process.env.PORT ||3000;
+app.set('view engine', 'ejs');
 
-app.get('/info')
+const users = [];
+
+app.use(express.urlencoded({ extended : false}));
+
+app.post('/register', async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password);
+        
+    } catch (error) {
+        
+    }
+});
+
+
+// define required routes starts
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs");
+});
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs");
+});
+// define required routes ends
 
 
 
+
+// configuring server
 app.listen(port, ()=>{
     console.log( "Your application is running on http://localhost:3000")});
